84c91bed16f9a15ee1a0fbed145f2b82c64f443f
김진기 <151550964+Jingik@users.noreply.github.com>
2024-01-29 23:55:33 +0900

    [Bronze V] Title: A+B - 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,11 @@
+import sys
+def input():
+    return sys.stdin.readline()
+
+while 1:
+    A,B = map(int, input().split())
+    if (A == 0) and (B == 0):
+        break
+    else:
+        print(A+B)
+        
