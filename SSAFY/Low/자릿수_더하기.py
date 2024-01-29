3106b4dde9652f1d843903ef15e7e84784af7e45
김진기 <kjg6462@naver.com>
2024-01-29 17:35:14 +0900

    new

--- SSAFY/Low/자릿수_더하기.py
+++
@@ -1,8 +0,0 @@
-import math
-T = list(input().strip())
-S = 0
-
-for i in T:
-    k = int(i)
-    S = S + k
-print(S)
--- SSAFY/Low/최대수_구하기.py
+++
@@ -1,9 +0,0 @@
-import math
-
-T = int(input())
-
-for i in range(1, T+1):
-    case = list(map(int, input().split()))
-    case = sorted(case)
-    Max = max(case)
-    print(f'#{i} {Max}')
