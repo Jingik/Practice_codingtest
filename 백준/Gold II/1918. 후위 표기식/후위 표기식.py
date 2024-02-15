414cb387987cb9a4b3cbd97fe3da47685335ca00
김진기 <151550964+Jingik@users.noreply.github.com>
2024-02-15 10:32:56 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold II/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold II/1918. 후위 표기식/후위 표기식.py
@@ -1,31 +1,25 @@
-def posfix(List):
-    isp = {'(':0, '*':2, '/':2, '+':1, '-':1} #스택 안에서의 우선순위
-    top = -1
-    stack  = [0]*len(List)
-    postfix = ''
-    for ch in List :
-        if ch.isalnum() :
-            postfix += ch
-        elif ch == '(' :
-            top += 1
-            stack[top] = ch
-        elif ch == ')' :
-            while top > -1 and stack[top] != '(' :
-                postfix += stack[top]
-                top -= 1
-            top -=1 # stack안의 '(' 제거
-        elif ch in isp :  #연산자인경우
-            while top > -1 and isp[stack[top]] >= isp[ch] :
-                postfix += stack[top]
-                top -= 1
-            top += 1
-            stack[top] = ch
+def postfix(List):
+    Speed = {"+": 1, "-": 1, "*": 2, "/": 2, "(" : 0}
+    text = ""
+    Stack = []
+    for i in List:
+        if i.isalnum():
+            text += i
+        else:
+            if i == "(":
+                Stack.append(i)
+            elif i == ")":
+                while len(Stack) and Stack[-1] != "(":
+                    text += Stack.pop()
+                Stack.pop()
+            elif i in Speed:
+                while len(Stack) and Speed[Stack[-1]] >= Speed[i]:
+                    text += Stack.pop()
+                Stack.append(i)
 
-    #stack에 남아 있는 연산자들 문자열에 붙이기
-    while top > -1 :
-        postfix += stack[top]
-        top -= 1
-    return postfix
+    while len(Stack) > 0:
+        text += Stack.pop()
+    return text
 
 List = input()
-print(posfix(List))
+print(postfix(List))
