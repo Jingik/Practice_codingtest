d1ea8ea1c78cfbb47b41690b3cc76e052bc5225a
김진기 <151550964+Jingik@users.noreply.github.com>
2024-02-21 00:48:47 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,20 @@
+def Dfs(idx):
+    visited.append(idx)
+    for node in Graph[idx]:
+        if node in visited:
+            continue    
+        Dfs(node)
+
+import sys
+input = sys.stdin.readline
+Node = int(input())
+N = int(input())
+visited = []
+Graph = {i : [] for i in range(Node+1)}
+for _ in range(N):
+    a, b = map(int, input().split())
+    Graph[a] += [b]
+    Graph[b] += [a]
+Dfs(1)
+
+print(len(visited) - 1)
