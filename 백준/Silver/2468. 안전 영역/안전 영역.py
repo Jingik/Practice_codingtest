dfe84126cfa7685da41137fb4716576fd38e9647
김진기 <151550964+Jingik@users.noreply.github.com>
2024-02-23 00:13:24 +0900

    [Silver I] Title: 안전 영역, Time: 1064 ms, Memory: 34172 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,44 +1,49 @@
 import sys
+from copy import deepcopy
+from collections import deque
+input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 
-#상 하 좌 우 변량
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+def isvalid(x,y,N):
+    if 0 <= x < N and 0 <= y < N:
+        return True
+    else:
+        return False
 
-#x,y 지점을 기준으로 주변을 탐색하는 재귀함수 
-def sink_DFS(x, y, h):
-    # x,y 좌표를 기준으로 상하좌우 좌표를 nx 포문으로 가져옴
-    for m in range(4):
-        nx = x + dx[m]
-        ny = y + dy[m]
-        # 자신이 건너갈 nx, ny 좌표에 대한 유효성을 먼저 검증함
-        if (0 <= nx < N) and (0 <= ny < N) and not sink_table[nx][ny] and water_board[nx][ny] > h:
-            #유효성이 검증된 좌표에 한해서 재귀함수를 호출. 이 과정이 없으면 쌓는 스택이
-            sink_table[nx][ny] = True
-            #실질적으로 재귀함수가 하는 역할은 sink_table에 boolean 값만 바꾸는 역할.
-            sink_DFS(nx, ny, h)
-
-
-N = int(sys.stdin.readline())
-water_board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-#입력값에 따른 물 높이 board 생성
-
-
-#물에 잠김 여부를 확인할 수 있는 Boolean Table 생성.
-# sink_table = [[False for i in range(N)] for j in range(N)]
-
-
-ans = 1
-for k in range(max(map(max, water_board))):
-    sink_table = [[False]*N for _ in range(N)]
-    count = 0
+def Bfs(List, N, number):
+    Queue = deque([List])
+    if Cp_board[List[0]][List[1]] > number:
+        Cp_board[List[0]][List[1]] = number
+    while Queue:
+        point = Queue.popleft()
+        for dir in direction:
+            nx = point[0] + dir[0]
+            ny = point[1] + dir[1]
+            if isvalid(nx,ny,N):
+                if Cp_board[nx][ny] > number:
+                    Cp_board[nx][ny] = number
+                    Queue.append([nx,ny])                    
+        
+N = int(input())
+Board = []
+maxNum = 0
+for i in range(N):
+    Board.append(list(map(int, input().split())))
+    for j in range(N):
+        if Board[i][j] > maxNum:
+            maxNum = Board[i][j]
+             
+Max = 0
+for x in range(maxNum):
+    Cp_board = deepcopy(Board)
+    Count = 0
     for i in range(N):
         for j in range(N):
-            if water_board[i][j] > k and not sink_table[i][j]:
-                count += 1
-                sink_table[i][j] = True
-                sink_DFS(i, j, k)
-    ans = max(ans, count)
-
-print(ans)
+            if Cp_board[i][j] > x:
+                Count += 1   
+                Bfs([i,j], N, x)
+    if Max < Count:
+        Max = Count
+print(Max)
