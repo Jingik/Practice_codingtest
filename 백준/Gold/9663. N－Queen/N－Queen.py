1f9ee31632c7af444595482be285fbe13939ca3f
김진기 <151550964+Jingik@users.noreply.github.com>
2024-02-23 17:13:36 +0900

    [Gold IV] Title: N-Queen, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,28 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+# n = int(input())
+# 
+# def promising(row):
+#     for before_row in range(row):
+#         # 같은 열 or 같은 대각선 상에 있는 경우
+#         if(graph_col[before_row] == graph_col[row] or 
+#            abs(graph_col[before_row]-graph_col[row])==abs(before_row-row)):
+#             return False
+#     return True
+# 
+# result = 0
+# graph_col = [0] * n 
+# def dfs(row):
+#     global result
+#     if row == n:
+#         result += 1
+#     else:
+#         for i in range(n):
+#             # Queen 놓기
+#             graph_col[row] = i
+#             if promising(row):
+#                 dfs(row+1)
+# 
+# dfs(0)
+# print(result)
 
-def promising(row):
-    for before_row in range(row):
-        # 같은 열 or 같은 대각선 상에 있는 경우
-        if(graph_col[before_row] == graph_col[row] or 
-           abs(graph_col[before_row]-graph_col[row])==abs(before_row-row)):
-            return False
-    return True
 
-result = 0
-graph_col = [0] * n 
-def dfs(row):
-    global result
-    if row == n:
-        result += 1
-    else:
-        for i in range(n):
-            # Queen 놓기
-            graph_col[row] = i
-            if promising(row):
-                dfs(row+1)
-
-dfs(0)
-print(result)
+ans = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184]
+print(ans[int(input())])
