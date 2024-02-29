642c8d9adccd2a38035d54351895c85802bbac0e
김진기 <151550964+Jingik@users.noreply.github.com>
2024-02-29 16:55:49 +0900

    [Silver II] Title: 창고 다각형, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,28 +1,26 @@
-N = int(input())  # 건물의 수 입력
+import sys
+input = sys.stdin.readline
 
-heights = [0] * 1001  # 각 위치의 건물 높이를 저장할 리스트
-max_h = 0  # 가장 높은 건물 높이
-max_i = 0  # 가장 높은 건물의 위치
-
-# 건물 정보 입력 및 처리
+N = int(input())
+heights = [0] * 1001
+Max_height = 0
+Max_Index = 0
+result = 0
 for _ in range(N):
-    l, h = map(int, input().split())
-    heights[l] = h  # 건물 위치에 해당하는 높이 저장
-    if max_h < h:
-        max_h = h
-        max_i = l
-
-# 좌측에서부터 최고 높이까지의 면적 계산
+    Index, height = map(int, input().split())
+    heights[Index] = height
+    if Max_height <= height:
+        Max_height = height
+        Max_Index = Index
 total_area = 0
 high_area = 0
-for i in range(max_i + 1):
+for i in range(Max_Index + 1):
     high_area = max(high_area, heights[i])
     total_area += high_area
 
-# 우측에서부터 최고 높이까지의 면적 계산
 high_area = 0
-for i in range(1000, max_i, -1):
+for i in range(1000, Max_Index, -1):
     high_area = max(high_area, heights[i])
     total_area += high_area
 
-print(total_area)  # 총 면적 출력
+print(total_area)  # 총 면적 출력    
