fdc49a554e601ce14a5324c7e9657305ded66d1b
김진기 <151550964+Jingik@users.noreply.github.com>
2024-03-12 23:47:23 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.py
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.py
@@ -1,39 +1,36 @@
 import sys
 
-input = sys.stdin.readline
+ap = -sys.maxsize
+an = sys.maxsize
 
-# 아래, 오른쪽만(최단 경로로)
-dy, dx = (1, 0), (0, 1)
+def DFS(y,x,p):
+    global ap, an
 
+    if y == n-1 and x == n-1:
+        ap = max(ap,p)
+        an = min(an,p)
+        return
 
-def dfs(i: int, j: int, curr_num: str, before: str):  # 좌표, 그때까지 계산한 값
-    global max_answer, min_answer
-
-    if i == N - 1 and j == N - 1:  # 학교 도착했으면
-        # 정답 업데이트
-        max_answer = max(max_answer, int(curr_num))
-        min_answer = min(min_answer, int(curr_num))
-
-    # 아래, 오른쪽만
-    for k in range(2):
-        ni = i + dy[k]
-        nj = j + dx[k]
-
-        # 범위
-        if ni < 0 or nj < 0 or ni >= N or nj >= N:
+    for i in range(2):
+        yy, xx = y + dy[i], x + dx[i]
+        if yy == n or xx == n:
             continue
+        if arr[y][x] == '*':
+            DFS(yy,xx,p*arr[yy][xx])
+        elif arr[y][x] == '+':
+            DFS(yy,xx,p+arr[yy][xx])
+        elif arr[y][x] == '-':
+            DFS(yy,xx,p-arr[yy][xx])
+        else:
+            DFS(yy,xx,p)
 
-        if map_arr[ni][nj].isdigit():  # 숫자라면
-            dfs(ni, nj, str(eval(curr_num + before + map_arr[ni][nj])), '')
-        else:  # 기호라면
-            dfs(ni, nj, curr_num, map_arr[ni][nj])
-
-
-N = int(input())
-map_arr = list(input().split() for _ in range(N))
-
-max_answer = -(5 ** 20)
-min_answer = 5 ** 20
-
-dfs(0, 0, map_arr[0][0], '')
-print(max_answer, min_answer)
+n = int(input())
+arr = [list(map(str,input().split())) for _ in range(n)]
+dy = [0, 1]
+dx = [1, 0]
+for i in range(n):
+    for j in range(n):
+        if (i+j)%2 == 0:
+            arr[i][j] = int(arr[i][j])
+DFS(0,0,arr[0][0])
+print(ap,an)
