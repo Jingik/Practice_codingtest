5d712e98d128ec28e84497006b7b15bf24e8b0af
김진기 <151550964+Jingik@users.noreply.github.com>
2024-06-25 14:15:18 +0900

    [Gold III] Title: 파일 합치기, Time: 2740 ms, Memory: 119212 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int, input().split()))
+    subtotal = [0]*(n+1)
+    subtotal[0]=0
+    for i in range(1, n+1): 
+        subtotal[i]=arr[i-1]
+        subtotal[i]+=subtotal[i-1]
+    dp=[ 
+        [1e9]*n
+        for _ in range(n)
+    ]
+    for i in range(n):
+        dp[i][i] = 0 
+    for i in range(2, n+1):
+        for j in range(n-i+1):
+            for k in range(j, j+i-1):
+                dp[j][j+i-1] = min(dp[j][j+i-1], dp[j][k]+dp[k+1][j+i-1]+subtotal[j+i]-subtotal[j]) 
+    
+    print(dp[0][n-1])
