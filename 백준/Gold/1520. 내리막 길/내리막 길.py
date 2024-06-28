4c823bcc6227b29d72fc589facd74d7546b66958
김진기 <151550964+Jingik@users.noreply.github.com>
2024-06-28 20:54:43 +0900

    [Gold III] Title: 내리막 길, Time: 116 ms, Memory: 42780 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,29 @@
 import sys
-sys.setrecursionlimit(10**6)
-inpurt = sys.stdin.readline
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
 
-def isvalid(x, y, M, N):
-    return 0 <= x < M and 0 <= y < N
-
-def dfs(x, y):
-    if x == M - 1 and y == N - 1:
+def dfs(sx, sy):
+    # 도착 지점에 도달하면 1(한 가지 경우의 수)를 리턴
+    if sx == m-1 and sy == n-1:
         return 1
-    if DP[x][y] != -1:
-        return DP[x][y]
 
-    ways = 0
-    for dir in Direction:
-        dx, dy = x + dir[0], y + dir[1]
-        if isvalid(dx, dy, M, N) and Map[x][y] > Map[dx][dy]:
-            ways += dfs(dx, dy)
+    # 이미 방문한 적이 있다면 그 위치에서 출발하는 경우의 수를 리턴
+    if dp[sx][sy] != -1:
+        return dp[sx][sy]
     
-    DP[x][y] = ways
-    return DP[x][y]
+    ways = 0
+    for i in range(4):
+        nx, ny = sx + dx[i], sy + dy[i]
+        if 0 <= nx < m and 0 <= ny < n and graph[sx][sy] > graph[nx][ny]:
+            ways += dfs(nx, ny)
+    
+    dp[sx][sy] = ways
+    return dp[sx][sy]
 
-Direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-M, N = map(int, input().split())
-Map = [list(map(int, input().split())) for _ in range(M)]
-DP = [[-1] * N for _ in range(M)]
 
-result = dfs(0, 0)
-print(result)
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
+dx, dy = [1,-1,0,0], [0,0,1,-1]
+
+print(dfs(0,0))
