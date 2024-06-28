a1dd0ab22e3ae0924d8f67cfca800417917e9289
김진기 <151550964+Jingik@users.noreply.github.com>
2024-06-28 21:12:13 +0900

    [Gold IV] Title: 문자열 폭발, Time: 700 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,21 +1,18 @@
-# 개선 답안
 import sys
+input = sys.stdin.readline
 
-# 입력값 처리
-S = sys.stdin.readline().rstrip()
-explosion_string = sys.stdin.readline().rstrip()
+Sentence = input().rstrip()
+explosion_string = input().rstrip()
 
-# stack으로 문자열 폭발 구현
 stack = []
 ex_len = len(explosion_string)
 
-for i in range(len(S)):
-    stack.append(S[i])
+for i in range(len(Sentence)):
+    stack.append(Sentence[i])
     if ''.join(stack[-ex_len:]) == explosion_string:
         for _ in range(ex_len):
             stack.pop()
 
-# 결과 출력
 if stack:
     print(''.join(stack))
 else:
