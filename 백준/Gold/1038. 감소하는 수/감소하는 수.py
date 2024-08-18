ca7be376dcd457fe0922c9d4f6a7ca367f7b434c
김진기 <151550964+Jingik@users.noreply.github.com>
2024-08-18 23:53:03 +0900

    Update 감소하는 수.py

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -7,7 +7,7 @@ for i in range(1, 11):
 	for j in combinations(range(10), i):
 		num = ''.join(list(map(str, reversed(list(j)))))
 		result.append(int(num))
-
+# .
 result.sort()
 if N >= len(result):
 	print(-1)
