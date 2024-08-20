696446f4d7518bb7fb30495edcad22a642c70e46
김진기 <151550964+Jingik@users.noreply.github.com>
2024-08-20 10:57:28 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,12 @@
+import sys
+
+input =sys.stdin.readline
+
+N = int(input())
+List = list(map(int, input().split()))
+Max = max(List)
+New_List = 0
+for i in range(N):
+    New_List += List[i] / Max * 100
+print(New_List/N)
+
