23955ad01221c35b7badddf6454a2479c25b714d
김진기 <151550964+Jingik@users.noreply.github.com>
2024-10-01 16:40:00 +0900

    [Gold V] Title: 숫자고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -3,21 +3,20 @@ input = sys.stdin.readline
 
 n = int(input())
 graph = [0] + [int(input()) for _ in range(n)]
-
-answer = []
 result = []
 
-def dfs(node, start):
+def dfs(node, resul):
     go = graph[node]
     visited[node] = True
     if not visited[go]:
-        dfs(go, start)
-    elif visited[go] and go == start:
-        result.append(go)
+        dfs(go, resul)
+    elif visited[go] and go == resul:
+        result.append(resul)
 
 for i in range(1, n+1):
     visited = [False] * (n+1)
-    dfs(i, i)
+    dfs(i,i)
+
 result.sort
 print(len(result))
-print(*result, sep="\n")
+print(*result, sep='\n')
