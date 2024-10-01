4e24778e7960d03e7f7a2c137c5a6bc9110cdc74
김진기 <151550964+Jingik@users.noreply.github.com>
2024-10-01 16:30:06 +0900

    [Gold V] Title: 숫자고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,32 +1,24 @@
-def dfs(v, table, visited, path):
-    visited[v] = True
-    path.append(v)
-    
-    next_v = table[v]  # 다음 이동할 노드
-    if not visited[next_v]:
-        return dfs(next_v, table, visited, path)
-    elif next_v in path:  # 사이클 발견
-        return path[path.index(next_v):]  # 사이클 부분만 반환
-    else:
-        return []
+import sys
+input = sys.stdin.readline
 
-# 입력 처리
-N = int(input())  # N 값 입력
-table = [0]  # 첫째 줄의 숫자들은 1부터 시작하므로 index 맞추기 위해 0 추가
-for _ in range(N):
-    table.append(int(input()))  # 둘째 줄의 숫자들 입력
+n = int(input())
+graph = [0] + [int(input()) for _ in range(n)]
+visited = [False] * (n+1)
 
-visited = [False] * (N + 1)
+answer = []
 result = []
 
-# 모든 노드에 대해 DFS 실행
-for i in range(1, N + 1):
-    if not visited[i]:
-        cycle = dfs(i, table, visited, [])
-        result.extend(cycle)
+def dfs(now, start):
+    go = graph[now]
+    visited[now] = True
+    if not visited[go]:
+        dfs(go, start)
+    elif visited[go] and go == start:
+        result.append(start)
 
-# 결과 출력
-result = sorted(set(result))  # 중복된 값 제거 후 정렬
-print(len(result))  # 뽑힌 정수의 개수 출력
-for num in result:
-    print(num)  # 뽑힌 정수들 출력
+for i in range(1, n+1):
+    visited = [False] * (n+1)
+    dfs(i, i)
+
+print(len(result))
+print(*sorted(result), sep="\n")
