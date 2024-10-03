d2f2029ac7e3e35770b7b90d2939d95ef2eba4dc
김진기 <151550964+Jingik@users.noreply.github.com>
2024-10-03 21:38:01 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,38 +1,26 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
-def bfs():
-    # BFS 큐
-    queue = deque([1])
-    visited[1] = True
-    cnt = 0
-    while queue:
-        for _ in range(len(queue)):
-            x = queue.popleft()
-            if x == 100:
-                return cnt
-            for i in range(1, 7):
-                next_pos = x + i
-                if next_pos <= 100:
-                    if next_pos in ladder_snake:
-                        next_pos = ladder_snake[next_pos]
-                    if not visited[next_pos]:
-                        visited[next_pos] = True
-                        queue.append(next_pos)
-        cnt += 1
-                    
-    return -1 
-
-# 사다리와 뱀 정보 입력
 N, M = map(int, input().split())
-ladder_snake = {}
+board = [i for i in range(101)] + [1] * 5
 
 for _ in range(N + M):
-    x, y = map(int, input().split())
-    ladder_snake[x] = y
+    start, end = map(int, input().split())
+    board[start] = end
 
-visited = [False] * 101 
-visited[0] = True
-print(bfs())
+visited = [False, True] + [False] * 99
+q = [1]
+cnt = 0
+
+while not visited[100]:
+    nq = []
+    cnt += 1
+
+    for n in q:
+        for i in range(1, 7):
+            next = board[n + i]
+
+            if not visited[next]:
+                visited[next] = True
+                nq.append(next)
+
+    q = nq
+
+print(cnt)
