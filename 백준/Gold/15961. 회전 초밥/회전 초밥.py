eb242f33f35e391708331ba6a6405a3a42ebaaf4
김진기 <151550964+Jingik@users.noreply.github.com>
2024-10-04 22:22:30 +0900

    [Gold IV] Title: 회전 초밥, Time: 2848 ms, Memory: 140952 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -1,36 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-N, d, k, c = map(int, input().split())
+N, D, K, C = map(int, input().split())
 belt = [int(input()) for _ in range(N)]
 
-count = [0] * (d + 1) 
-total = 0 
-variety = 0 
+# 초밥 가짓수 
+variety = [0] * (D + 1)
+variety[C] = 1
+# 무조건 쿠폰은 먹음
+cnt = 1
+result = 1
 
-for i in range(k):
-    if count[belt[i]] == 0: 
-        variety += 1
-    count[belt[i]] += 1
+for k in range(K):
+    if variety[belt[k]] == 0:
+        cnt += 1
+    
+    variety[belt[k]] += 1
 
-total = variety
-if count[c] == 0:
-    total += 1
+for n in range(N):
+    result = max(result, cnt)
 
-for i in range(1, N):
+    variety[belt[n]] -= 1
+    if variety[belt[n]] == 0:
+        cnt -= 1
+    
+    if variety[belt[(n + K) % N]] == 0:
+        cnt += 1
+    variety[belt[(n + K) % N]] += 1
 
-    outgoing_sushi = belt[i - 1]
-    count[outgoing_sushi] -= 1
-    if count[outgoing_sushi] == 0: 
-        variety -= 1
-        
-    incoming_sushi = belt[(i + k - 1) % N]
-    if count[incoming_sushi] == 0:
-        variety += 1
-    count[incoming_sushi] += 1
-    current_total = variety
-    if count[c] == 0: 
-        current_total += 1
-    total = max(total, current_total)
-
-print(total)
+print(result)
