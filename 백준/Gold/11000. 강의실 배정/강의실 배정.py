eec8594a367a8657c5c9ee87800cc9b3e72c49d0
김진기 <151550964+Jingik@users.noreply.github.com>
2024-10-05 09:50:43 +0900

    [Gold V] Title: 강의실 배정, Time: 324 ms, Memory: 74228 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())  
+classes = [list(map(int, input().split())) for _ in range(N)]
+
+classes.sort(key=lambda x: x[0])
+
+heap = []
+
+heapq.heappush(heap, classes[0][1])
+
+for i in range(1, N):
+    if heap[0] <= classes[i][0]:
+        heapq.heappop(heap)
+
+    heapq.heappush(heap, classes[i][1])
+
+print(len(heap))
