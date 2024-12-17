e6d7d90767a9cabaf8bb5b1e0442e8012b3bf0c7
김진기 <151550964+Jingik@users.noreply.github.com>
2024-12-17 18:00:12 +0900

    [Gold III] Title: 드래곤 커브, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -11,33 +11,29 @@ input = sys.stdin.readline
 
 n = int(input())
 
+Dircetion = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 graph = [[0] * 101 for _ in range(101)]
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
 
-for i in range(n):
+for _ in range(n):
     y, x, d, g = map(int, input().split())
     graph[x][y] = 1
-
-    # 커브 리스트 만들기
     curve = [d]
-    for j in range(g):
-        for k in range(len(curve) - 1, -1, -1):
-            curve.append((curve[k] + 1) % 4)
-
-    # 드래곤 커브 만들기
-    for j in range(len(curve)):
-        x += dx[curve[j]]
-        y += dy[curve[j]]
-        if x < 0 or x >= 101 or y < 0 or y >= 101:
+    # len(curve) ^
+    for _ in range(g):
+        for i in range(len(curve)-1, -1, -1):
+            curve.append((curve[i] + 1) % 4)
+    
+    for i in range(len(curve)):
+        x += Dircetion[curve[i]][0]
+        y += Dircetion[curve[i]][1]
+        if  x < 0 or x > 100 or y < 0 or y > 100 :
             continue
-
         graph[x][y] = 1
 
 answer = 0
-for i in range(100):
-    for j in range(100):
-        if graph[i][j] == 1 and graph[i + 1][j] == 1 and graph[i][j + 1] == 1 and graph[i + 1][j + 1] == 1:
+for x in range(100):
+    for y in range(100):
+        if graph[x][y] == 1 and graph[x][y + 1] == 1 and graph[x + 1][y] == 1 and graph[x+1][y+1] == 1:
             answer += 1
-
+            
 print(answer)
