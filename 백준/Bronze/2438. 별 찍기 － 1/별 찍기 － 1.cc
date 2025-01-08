dfcabb72d598ef3e7f9079bce43f007397fb13ad
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-08 16:12:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+    //cout << "Hello World!\n";
+}
