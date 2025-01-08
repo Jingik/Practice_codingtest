fe610707abd94ff0fc878b27c78338aab8f75d60
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-08 16:08:39 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    cout << fixed << setprecision(10) << a / b;
+
+    return 0;
+    //cout << "Hello World!\n";
+}
