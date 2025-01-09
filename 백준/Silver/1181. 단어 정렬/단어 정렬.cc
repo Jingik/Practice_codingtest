5a036ded4b25d746f58400ff79fe24a71feb8cd9
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-09 16:07:17 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3848 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,36 +1,38 @@
+// 01_09.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
 #include <iostream>
 #include <string>
-#include <algorithm> 
+#include <algorithm>
+#include <vector>
+#include <set>
+
 using namespace std;
 
-
-bool compare(const string &a, const string &b) {
-    if (a.length() != b.length()) {
-        return a.length() < b.length(); 
+bool compare(const string& a, const string& b) {
+    if (a.length() == b.length()) {
+        return a < b;
     }
-    return a < b;
+    return a.length() < b.length();
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int N;
-    cin >> N;
+    int num;
+    cin >> num;
 
-    string words[20000]; 
-
-    for (int i = 0; i < N; i++) {
-        cin >> words[i];
+    string exam[20000];
+    for (int i = 0; i < num; i++) {
+        cin >> exam[i];
     }
-
-    sort(words, words + N, compare);
-
-    for (int i = 0; i < N; i++) {
-        if (i == 0 || words[i] != words[i - 1]) { 
-            cout << words[i] << '\n';
+    sort(exam, exam + num, compare);
+    
+    for (int i = 0; i < num; i++) {
+        if (i == 0 || exam[i] != exam[i - 1]) {
+            cout << exam[i] << '\n';
         }
     }
-
     return 0;
 }
