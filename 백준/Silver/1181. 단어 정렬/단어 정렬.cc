06ed02b12f8fa9366577ebadb5c7706f748b0551
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-09 15:38:32 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <vector>
-#include <set>
-
+#include <algorithm> 
 using namespace std;
 
-bool compare(const string& a, const string& b) {
-    if (a.length() == b.length()) {
-        return a < b;
+
+bool compare(const string &a, const string &b) {
+    if (a.length() != b.length()) {
+        return a.length() < b.length(); 
     }
-    return a.length() < b.length();
+    return a < b;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int num;
-    cin >> num;
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
 
-    vector<string> words(num);
-    for (int i = 0; i < num; i++) {
+    int N;
+    cin >> N;
+
+    string words[20000]; 
+
+    for (int i = 0; i < N; i++) {
         cin >> words[i];
     }
 
-	set<string> unique_words(words.begin(), words.end());
-	words.assign(unique_words.begin(), unique_words.end());
+    sort(words, words + N, compare);
 
-    sort(words.begin(), words.end(), compare);
-
-    for (const string& word : words) {
-        cout << word << endl;
+    for (int i = 0; i < N; i++) {
+        if (i == 0 || words[i] != words[i - 1]) { 
+            cout << words[i] << '\n';
+        }
     }
 
     return 0;
